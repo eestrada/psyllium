744ab966f094832626ddcd9a27be0d1b5d406c22
Ethan Estrada <ethan@misterfidget.com>
2025-02-21 23:07:25 -0600

    Add `status` method to `Husk`
    Correct more behavior to be like `Thread`.

--- lib/psyllium.rb
+++ lib/psyllium.rb
@@ -11,24 +11,26 @@ module Psyllium
 
   # Wrap Exception instances for propagation
   class ExceptionalCompletionError < Error
-    attr_reader :internal_exception
-
     def initialize(expt)
       @internal_exception = expt
       super(internal_exception)
     end
+
+    def cause
+      @internal_exception
+    end
   end
 
-  # Class that wraps a `Fiber` to make it easier to treat it like a `Thread` or
-  # some sort of Future or Promise.
+  # Class that wraps a `Fiber` to make it easier to treat it like a `Thread`.
   #
   # Using this class only makes sense when a Fiber scheduler has been set and
   # when the caller does not intend to interact directly with Fiber execution
   # using primitive operations such as `Fiber.resume` and `Fiber.yield`.
   #
-  # The internally created `Fiber` cannot be directly accessed. However, it is
-  # possible to check if it is `alive?` using `Husk.alive?` and to forcefully
-  # raise an exception within it by calling `Husk.raise`.
+  # The internally created `Fiber` cannot be directly accessed. However, many
+  # of its methods are forwarded, including `alive?`, `raise`, `kill`,
+  # `backtrace`, and `backtrace_locations`. Methods are only forwarded if they
+  # are identical to the ones found on `Thread`.
   class Husk
     include Timeout
     extend Forwardable
@@ -67,74 +69,46 @@ module Psyllium
       end
     end
 
-    # Return `true` if the block completed executing, `false` otherwise.
-    def complete?
-      !alive?
-    end
-
-    # Raises an `Psyllium::IncompleteError` exception if execution of the block
-    # has not completed yet.
-    #
-    # Otherwise, does nothing.
-    def check_complete!
-      raise IncompleteError unless complete?
-    end
-
-    # Return the value returned from the given block. May be `nil` either
-    # because the given block returned `nil` or because the block raised an
-    # exception. Use `exceptional_completion?` to check if the block completed
-    # exceptionally.
-    #
-    # Raises an `Psyllium::IncompleteError` exception if execution of the block
-    # has not completed yet.
+    # Waits for Husk to complete, using join, and returns its value. If Husk
+    # completed with an exception, raises ExceptionalCompletionError exception,
+    # with the original exception as its `cause`.
     def value
-      check_complete!
-      raise ExceptionalCompletionError.new(exception) if exceptional_completion?
+      join
+      raise ExceptionalCompletionError.new(@exception) if @exception
 
       @value
     end
 
-    # Return `true` if the block completed exceptionally, `false` otherwise.
-    #
-    # Raises an `Psyllium::IncompleteError` exception if execution of the block
-    # has not completed yet.
-    def exceptional_completion?
-      check_complete!
-      !!@exception
+    def status
+      if alive?
+        # The only possible state this can be in is "sleeping" when alive since
+        # the Husk lives in the parent Fiber. If the parent Fiber is running,
+        # the child Fiber held in `@fiber` cannot (by definition) be running,
+        # it must be sleeping.
+        'sleeping'
+      elsif @exception
+        nil
+      else
+        false
+      end
     end
 
-    # Return the exception raised from the given block. Returns `nil` if no
-    # exception was raised.
-    #
-    # Raises an `Psyllium::IncompleteError` exception if execution of the block
-    # has not completed yet.
-    def exception
-      check_complete!
-      @exception
-    end
-
-    # Wait until execution completes. Return the value of the executed block,
-    # if it completed successfully.
-    #
-    # Can be given a `wait_timeout` value, in which case execution will end
-    # early with a TimeoutError. By default, this will wait indefinitely for
-    # execution to complete.
-    #
-    # By default this will raise an `ExceptionalCompletionError` exception if
-    # the Husk instance raised an exception during execution. Setting the
-    # keyword argument `raise_exception` to `false` will suppress this
-    # behavior. The exception (if raised) can still be retrieved afterward via
-    # the `exception` instance method.
+    # Wait until execution completes. Return the Husk instance. If `limit` is
+    # reached, returns `nil` instead.
     #
     # `join` may be called more than once.
-    def join(wait_timeout = nil, raise_exception: true)
-      timeout(wait_timeout) do
+    def join(limit = nil)
+      timeout(limit) do
+        # Once this mutex finishes synchronizing, that means the initial
+        # calculation is done and we can return `self`, which is the Husk
+        # instance.
         @mutex.synchronize do
-          raise ExceptionalCompletionError.new(exception) if raise_exception && exceptional_completion?
-
-          value
+          self
         end
       end
+    rescue Timeout::Error
+      # mimic Thread behavior by returning `nil` on timeout.
+      nil
     end
   end
 end
