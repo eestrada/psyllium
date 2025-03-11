065b88501ba37ef37399829607ee088704430179
Ethan Estrada <ethan@misterfidget.com>
2025-03-11 12:58:24 -0500

    Tighten benchmark threshold
    Since it is testing for constant time.

--- test/test_benchmarks.rb
+++ test/test_benchmarks.rb
@@ -7,7 +7,7 @@ require_relative 'test_helper'
 class BenchPsyllium < Minitest::Benchmark
   def bench_joining_multiple_fibers_is_roughly_big_o_constant
     Async do
-      assert_performance_constant(0.99) do |n|
+      assert_performance_constant(0.9999) do |n|
         sleep_time = 0.01
         fibers = n.times.map { Fiber.start { sleep(sleep_time) } }
         fibers.each(&:join)
