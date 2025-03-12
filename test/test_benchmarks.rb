b9b672dbfedb73ea8dbbe0049b3f9909f1374040
Ethan Estrada <ethan@misterfidget.com>
2025-03-12 17:01:02 -0500

    Increase the sleep time of the benchmark

--- test/test_benchmarks.rb
+++ test/test_benchmarks.rb
@@ -8,7 +8,7 @@ class BenchPsyllium < Minitest::Benchmark
   def bench_joining_multiple_fibers_is_roughly_big_o_constant
     Async do
       assert_performance_constant(0.9999) do |n|
-        sleep_time = 0.01
+        sleep_time = 0.1
         fibers = n.times.map { Fiber.start { sleep(sleep_time) } }
         fibers.each(&:join)
       end
